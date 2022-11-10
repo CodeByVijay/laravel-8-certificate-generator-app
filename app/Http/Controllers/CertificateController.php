162f5eda7b5f168da4fcff1eff9ee42c93e7640f
CodeByVijay <gitbyvijay@gmail.com>
2022-11-10 14:16:41 +0530

    first commit

---
+++ app/Http/Controllers/CertificateController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+// use Intervention\Image\Image;
+
+class CertificateController extends Controller
+{
+
+
+    public function makeimage($name = "Vijay Amule")
+    {
+        // $name = "Vijay Amule";
+        $date = date('d M Y');
+        $sign = "Vijay Amule";
+        $cer_no = rand(000000000, 999999999);
+        $img = Image::make(public_path('image/certificate.png'));
+        $img->text($name, 1050, 680, function ($font) {
+            $font->file(public_path('image/font.ttf'));
+            $font->size(50);
+            $font->color('#c62e46');
+            $font->align('center');
+            $font->valign('bottom');
+            $font->angle(0);
+        });
+
+        $img->text($date, 570, 1190, function ($font) {
+            $font->file(public_path('image/font.ttf'));
+            $font->size(50);
+            $font->color('#c62e46');
+            $font->align('center');
+            $font->valign('bottom');
+            $font->angle(0);
+        });
+
+        $img->text($sign, 1450, 1190, function ($font) {
+            $font->file(public_path('image/signfont.otf'));
+            $font->size(50);
+            $font->color('#c62e46');
+            $font->align('center');
+            $font->valign('bottom');
+            $font->angle(0);
+        });
+        $img->text($cer_no, 1000, 1350, function ($font) {
+            $font->file(public_path('image/font.ttf'));
+            $font->size(50);
+            $font->color('#c62e46');
+            $font->align('center');
+            $font->valign('bottom');
+            $font->angle(0);
+        });
+        $img->insert(public_path('image/qr.png'), 'bottom-left', 450, 450);
+        $img->save(public_path('image/certificate1.png'));
+        return view('cerificate');
+    }
+}
---
+++ resources/views/cerificate.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Certificate</title>
+</head>
+<body>
+<div class="container mt-5 mb-5">
+    <img src="{{asset('image/certificate1.png')}}" alt="" width="800" height="600">
+    <br>
+    <a href="{{asset('image/certificate1.png')}}" download="certificate.png">Download</a>
+</div>
+</body>
+</html>
